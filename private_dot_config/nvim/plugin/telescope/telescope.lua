48ea3c9834cd27637001bd2eac6f4e7ea60d8467
luatil-cloud <lua.santilli@cloudwalk.io>
2024-07-28 17:47:32 -0300

    nvim: lazy.nvim for base config

--- private_dot_config/nvim/plugin/telescope/telescope.lua
+++
@@ -1,9 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-g>', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<C-f>', builtin.current_buffer_fuzzy_find, {})
-vim.keymap.set('n', '<C-F>', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
--- vim.keymap.set('n', '/', builtin.current_buffer_fuzzy_find, { noremap = false })
